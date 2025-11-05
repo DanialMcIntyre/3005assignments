3398f7fbaa61b373fd4db2dc1dbeb570eed05625
Danie <31494242+DanialMcIntyre@users.noreply.github.com>
2025-11-05 15:46:53 -0500

    Create create_student_table.sql

---
+++ assignment3/scripts/create_student_table.sql
@@ -0,0 +1,14 @@
+--Create table
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    enrollment_date DATE
+);
+
+--Insert into table
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
+('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
+('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');
